8a6a1d71ee9b3dc3d8d6e3c6a3b3493cba2f8783
kburak <burakkucuk@yahoo.com>
2021-12-12 14:41:56 +0100

    Add withStyles with demo styles

---
+++ src/MiniPalette.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { withStyles } from '@mui/styles';
+
+const styles = {
+    main: {
+        backgroundColor: 'purple',
+        border: '3px solid teal'
+    },
+    secondary: {
+        backgroundColor: 'pink',
+        '& h1': {
+            color: 'white',
+            '& span': {
+                backgroundColor: 'yellow'
+            }
+        }
+    }
+};
+
+function MiniPalette(props) {
+    const { classes } = props;
+    console.log(classes);
+    return (
+        <div className={classes.main}>
+            <h1>Mini Palette</h1>
+            <section className={classes.secondary}>
+                <h1>
+                    Mini Palette <span>testing</span>
+                </h1>
+            </section>
+            <span>testing 2</span>
+        </div>
+    )
+}
+
+export default withStyles(styles)(MiniPalette);
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import MiniPalette from './MiniPalette';
 
 class Palettelist extends Component {
     render() {
         console.log(this.props.list);
         return (
             <div>
+                <MiniPalette />
                 <h1>React Colors</h1>
                 {this.props.list.map(palette => (
                     <p>
