85eaf6aa05e52926cdc5be5452099c1951a9a70e
kburak <burakkucuk@yahoo.com>
2021-12-12 14:55:33 +0100

    Start styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,33 +2,44 @@ import React from 'react';
 import { withStyles } from '@mui/styles';
 
 const styles = {
-    main: {
-        backgroundColor: 'purple',
-        border: '3px solid teal'
-    },
-    secondary: {
-        backgroundColor: 'pink',
-        '& h1': {
-            color: 'white',
-            '& span': {
-                backgroundColor: 'yellow'
-            }
+    root: {
+        backgroundColor: 'white',
+        border: '1px solid black',
+        borderRadius: '5px',
+        padding: '0.5rem',
+        position: 'relative',
+        overflow: 'hidden',
+        '& hover': {
+            cursor: 'pointer'
         }
+    },
+    colors: {
+        backgroundColor: 'white'
+    },
+    title: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        margin: '0',
+        color: 'black',
+        paddingTop: '0.5rem',
+        fontSize: '1rem',
+        position: 'relative'
+    },
+    emoji: {
+        marginLeft: '0.5rem',
+        fontSize: '1.5rem'
     }
 };
 
 function MiniPalette(props) {
-    const { classes } = props;
+    const { classes, paletteName, emoji } = props;
     console.log(classes);
     return (
-        <div className={classes.main}>
-            <h1>Mini Palette</h1>
-            <section className={classes.secondary}>
-                <h1>
-                    Mini Palette <span>testing</span>
-                </h1>
-            </section>
-            <span>testing 2</span>
+        <div className={classes.root}>
+            <div className={classes.colors}>
+                <h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
+            </div>
         </div>
     )
 }
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -4,15 +4,11 @@ import MiniPalette from './MiniPalette';
 
 class Palettelist extends Component {
     render() {
-        console.log(this.props.list);
         return (
             <div>
-                <MiniPalette />
                 <h1>React Colors</h1>
                 {this.props.list.map(palette => (
-                    <p>
-                    <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-                    </p>
+                    <MiniPalette {...palette}/>
                 ))}
             </div>
         );
