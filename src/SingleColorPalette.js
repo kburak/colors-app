34288d652b9456bc207e4d686a3cb0c07f66269e
kburak <burakkucuk@yahoo.com>
2021-12-19 16:31:05 +0100

    Add Navbar and Footer to SingleColorPalette

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,34 +1,54 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
 import ColorBox from './ColorBox';
+import PaletteFooter from './PaletteFooter';
 
 class SingleColorPalette extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+        this.state = {
+            format: "hex"
+        }
+        this.changeFormat = this.changeFormat.bind(this);
     }
-    gatherShades(palette, color){
+    gatherShades(palette, color) {
         //return all shades of given color
-        let shades= [];
+        let shades = [];
         let allColors = palette.colors;
-        for(let key in allColors){
-            for(let level of allColors[key]){
-                if(level.id === color){
+        for (let key in allColors) {
+            for (let level of allColors[key]) {
+                if (level.id === color) {
                     shades.push(level);
                 }
             }
         }
         return shades.slice(1);
     }
+    changeFormat(val) {
+        this.setState({ format: val });
+    }
     render() {
+        const { format } = this.state;
+        const { paletteName, emoji } = this.props.palette;
         const colorBoxes = this._shades.map(color => (
-            <ColorBox key={color.name} name={color.name} background={color.hex} showLink={false}/>
+            <ColorBox
+                key={color.name}
+                name={color.name}
+                background={color[format]}
+                showLink={false}
+            />
         ));
         return (
             <div className="Palette">
-                <h1>Single color palette!</h1>
+                <Navbar
+                    handleChange={this.changeFormat}
+                    showingAllColors={false}
+                />
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
+                <PaletteFooter paletteName={paletteName} emoji={emoji} />
             </div>
         );
     }
